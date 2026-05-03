9b792b492efd503b915695024196de38fee7f798
John Rose <johnedmundrose@gmail.com>
2026-05-02 20:13:36 -0400

    Add prototype routes

--- code/config/routes.rb
+++ code/config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "forums#index"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get    "signup",  to: "registrations#new",  as: :signup
+  post   "signup",  to: "registrations#create"
+  get    "login",   to: "sessions#new",       as: :login
+  post   "login",   to: "sessions#create"
+  delete "logout",  to: "sessions#destroy",   as: :logout
+
+  resources :forums, only: [:index, :show, :new, :create] do
+    resources :posts, only: [:create]
+  end
+
+  resources :posts, only: [:show] do
+    resources :posts, only: [:create], path: :replies, as: :replies
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
